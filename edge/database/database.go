6dec6fe610c2d9aaf18202a2010ac3afb4f3134d
pli <chordplaylist@gmail.com>
2024-08-27 00:44:37 +0900

    feat: set database

--- edge/database/database.go
+++ edge/database/database.go
@@ -8,14 +8,17 @@ import (
 	_ "github.com/go-sql-driver/mysql"
 )
 
-func main() {
-	client, err := ent.Open("mysql", "<user>:<pass>@tcp(<host>:<port>)/<database>?parseTime=True")
+func InitDB() *ent.Client {
+	dsn := "root:0000@tcp(localhost:3306)/todo?parseTime=True"
+	client, err := ent.Open("mysql", dsn)
 	if err != nil {
 		log.Fatalf("failed opening connection to mysql: %v", err)
 	}
-	defer client.Close()
+	//defer client.Close()
 	// Run the auto migration tool.
 	if err := client.Schema.Create(context.Background()); err != nil {
 		log.Fatalf("failed creating schema resources: %v", err)
 	}
+
+	return client
 }
