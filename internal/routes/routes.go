1675be61a3c469d9d438dc7a91bc963dc28f0ff8
pli <chordplaylist@gmail.com>
2024-08-28 00:21:23 +0900

    feat: update api path

--- internal/routes/routes.go
+++ internal/routes/routes.go
@@ -5,7 +5,7 @@ import (
 )
 
 func RegisterRoutes(r *chi.Mux) {
-	r.Mount("/todos", TodoRoutes())
-	r.Mount("/users", UserRoutes())
+	r.Mount("/api/v1/todos", TodoRoutes())
+	r.Mount("/api/v1/users", UserRoutes())
 	// 새로운 라우트를 추가하려면 여기서 r.Mount()를 호출합니다.
 }
