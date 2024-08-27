5b961b3416894e60123919461b5d123fc4f17025
pli <chordplaylist@gmail.com>
2024-08-28 01:10:05 +0900

    feat: update todo model sql

--- migration/migration.sql
+++ migration/migration.sql
@@ -1,7 +1,11 @@
-
-create table todos (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    title VARCHAR(255) NOT NULL,
+create table todos
+(
+    id          INT AUTO_INCREMENT PRIMARY KEY,
+    title       VARCHAR(255)                              NOT NULL,
     description TEXT,
-    status enum('PENDING', 'COMPLETED', 'PROGRESS') NOT NULL,
-)
+    status      enum ('PENDING', 'COMPLETED', 'PROGRESS') NOT NULL,
+    created_at  DATETIME                                  NOT NULL,
+    updated_at  DATETIME                                  NOT NULL,
+    deleted_at  DATETIME
+) CHARACTER SET utf8mb4
+  COLLATE utf8mb4_unicode_ci;
